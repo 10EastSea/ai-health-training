549bc5fe978175c40a44a03b66435b06753d38b5
10EastSea <xgf6580@gmail.com>
2021-01-24 12:14:22 +0000

    test2_model 3 add

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,9 +10,9 @@ function Navigation() {
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <NavDropdown title="Exercise Menu" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="lunge.html">Lunge</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/2.2">Push Up</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/2.3">Squat</NavDropdown.Item>
+                        <NavDropdown.Item href="./exercise_model/lunge.html">Lunge</NavDropdown.Item>
+                        <NavDropdown.Item href="./exercise_model/pushup.html">Push Up</NavDropdown.Item>
+                        <NavDropdown.Item href="./exercise_model/squat.html">Squat</NavDropdown.Item>
                         <NavDropdown.Item href="#action/2.4">Sit Up</NavDropdown.Item>
                         <NavDropdown.Item href="#action/2.5">Shoulder Press</NavDropdown.Item>
                         <NavDropdown.Item href="#action/2.6">Leg Raise</NavDropdown.Item>
