938799a9f048d82cbf3e59edd633b7fd6ab0ce32
10EastSea <xgf6580@gmail.com>
2021-01-24 08:57:59 +0000

    test1

---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Navigation.css";
+
+function Navigation() {
+    return (
+        <div className="Navigation">
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">AI Health Trainer</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <NavDropdown title="Exercise Menu" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="lunge.html">Lunge</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/2.2">Push Up</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/2.3">Squat</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/2.4">Sit Up</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/2.5">Shoulder Press</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/2.6">Leg Raise</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
+        </div >
+    );
+}
+
+export default Navigation;
