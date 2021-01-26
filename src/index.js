31ba4da46ebc0419a4796230594d4c756d9e818c
10EastSea <xgf6580@gmail.com>
2021-01-26 09:37:35 +0000

    molla sibal 4

--- src/index.js
+++ src/index.js
@@ -129,7 +129,7 @@ class App extends React.Component {
 }
 
 
-ReactDOM.render(<Navigation />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
